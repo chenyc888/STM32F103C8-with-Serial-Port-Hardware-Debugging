360d1cf25b33331534eeab2b4d8a449be8a56d08
YC Chen <chenyc888@yahoo.com>
2021-04-02 21:05:49 -0700

    Update codes with Maple core

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(9600); //default serial port is usart1 with TX=A9 and RX=A10. so, connect GND, A9 and A10 of STM32F103C8 to FTDI will establish a valid serial port. likewise,
-                      //connect A9 and A10 to a bluetooth module will port the serial output via bluetooth. so, stm32 boards can achieve the same thing as an avr board.
+  Serial3.begin(9600); //default serial port is usart1 with TX=A9 and RX=A10. so, connect GND, A9 and A10 of STM32F103C8 to FTDI will establish a valid serial port. likewise,
+                       //connect A9 and A10 to a bluetooth module will port the serial output via bluetooth. so, stm32 boards can achieve the same thing as an avr board.
+                       //note that Serial1 does not work with maple core. so, need to switch to other serial pins such as Serial3, which is TX=B10 and RX=B11
 }
 uint8_t i = 0;
 void loop()
 {
   // put your main code here, to run repeatedly:
   i++;
-  Serial.println("hello world"); //with settings in platformIO.ini, this code allows debugging with ST Link programmer.
+  Serial3.println("hello world"); //with settings in platformIO.ini, this code allows debugging with ST Link programmer.
 
   delay(1000);
 }
